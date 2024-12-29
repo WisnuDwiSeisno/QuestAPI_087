3dece7f3be2e5c58dbc520eae6b2bef677c78bce
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-29 18:27:16 +0700

    menambahkan HomeViewModel untuk mengelola state mahasiswa dan memanggil data mahasiswa.

--- app/src/main/java/com/example/onlinedatabase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/onlinedatabase/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.onlinedatabase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.onlinedatabase.Repository.MahasiswaRepository
 import com.example.onlinedatabase.model.Mahasiswa
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+    private set
+
+    init {
+        getMhs()
+    }
 }
