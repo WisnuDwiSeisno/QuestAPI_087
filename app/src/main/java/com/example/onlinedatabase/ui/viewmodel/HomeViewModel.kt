017ed56ca6ece24bc6131acbcca0530873cf3437
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-29 18:36:19 +0700

    menambahkan logika pengambilan data mahasiswa dengan penanganan error.

--- app/src/main/java/com/example/onlinedatabase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/onlinedatabase/ui/viewmodel/HomeViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.onlinedatabase.ui.viewmodel
 
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.onlinedatabase.Repository.MahasiswaRepository
 import com.example.onlinedatabase.model.Mahasiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -13,11 +19,26 @@ sealed class HomeUiState {
     object Loading : HomeUiState()
 }
 
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
     var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
     private set
 
     init {
         getMhs()
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            }catch (e: IOException){
+                HomeUiState.Error
+            }catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
     }
 }
