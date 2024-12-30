415c6a5d50b572af46189a89dd6e2324f008258d
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-30 09:06:16 +0700

    menambahkan EntryMhsScreen untuk mengelola UI entri mahasiswa dengan navigasi kembali.

--- app/src/main/java/com/example/onlinedatabase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/onlinedatabase/ui/view/InsertView.kt
@@ -1,8 +1,22 @@
 package com.example.onlinedatabase.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.onlinedatabase.ui.nagivation.DestinasiNavigasi
+import com.example.onlinedatabase.ui.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit, modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
