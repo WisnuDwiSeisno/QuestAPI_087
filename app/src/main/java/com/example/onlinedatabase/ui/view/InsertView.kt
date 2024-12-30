409adec3f0af8f2e659733fad7a119c7d3228298
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-30 09:00:26 +0700

    objek DestinasiEntry untuk rute navigasi 'item_entry' dengan judul 'Entry Mhs'.

---
+++ app/src/main/java/com/example/onlinedatabase/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.onlinedatabase.ui.view
+
+import com.example.onlinedatabase.ui.nagivation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
